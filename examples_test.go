18688663934064a8b429f67d1e581a5ae92ecebe
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-20 16:42:45 -0700

    Add a round-trip package-level example.

--- examples_test.go
+++ examples_test.go
@@ -48,3 +48,40 @@ func Example() {
 	// Applied:
 	// Grade: A-
 }
+
+func Example_roundTrip() {
+	p := pattern.MustParse(`git@${host}:${user}/${repo}.git`, pattern.Binds{
+		{Name: "host", Expr: `\w+(\.\w+)*`},
+		{Name: "user", Expr: `\w+`},
+		{Name: "repo", Expr: `\w+`},
+	})
+
+	const input = `git@bitbucket.org:creachadair/stringset.git`
+	m, err := p.Match(input)
+	if err != nil {
+		log.Fatalln("Match:", err)
+	}
+
+	// Apply the bindings to a derived pattern.
+	d, err := p.Derive(`http://${host}/${user}/${repo}`)
+	if err != nil {
+		log.Fatalln("Derive:", err)
+	}
+	do, err := d.Apply(m)
+	if err != nil {
+		log.Fatalln("Apply:", err)
+	}
+
+	// Applying the same bindings to the original pattern should recover the
+	// input string.
+	oo, err := p.Apply(m)
+	if err != nil {
+		log.Fatalln("Apply:", err)
+	}
+
+	fmt.Println(" derived: ", do)
+	fmt.Println(" original:", oo, oo == input)
+	// Output:
+	//  derived:  http://bitbucket.org/creachadair/stringset
+	//  original: git@bitbucket.org:creachadair/stringset.git true
+}
