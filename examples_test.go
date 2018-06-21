cbc3342b8295904350a472cd548c100fffcd9ad7
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-20 19:15:47 -0700

    Be consistent in examples about using Binds.

--- examples_test.go
+++ examples_test.go
@@ -10,7 +10,7 @@ import (
 func Example() {
 	// A pattern consists of a template string containing pattern words, and a
 	// set of bindings that give regular expressions that each word must match.
-	p := pattern.MustParse(`Grade: ${grade}`, []pattern.Bind{
+	p := pattern.MustParse(`Grade: ${grade}`, pattern.Binds{
 		{Name: "grade", Expr: `([ABCD][-+]?|[EF])`},
 	})
 
@@ -30,7 +30,7 @@ func Example() {
 	// In addition, you can substitute bindings back into a pattern to produce
 	// a new string. Bindings not mentioned by p are ignored, but all the
 	// bindings that p uses must be provided.
-	s, err := p.Apply([]pattern.Bind{{
+	s, err := p.Apply(pattern.Binds{{
 		Name: "grade",
 		Expr: "A-",
 	}})
