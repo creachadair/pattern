72f584e18451d8b38b6d995f2f9c04b48738f52d
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-24 11:05:35 -0700

    Convert the package example to a Parse example.

--- examples_test.go
+++ examples_test.go
@@ -7,46 +7,22 @@ import (
 	"bitbucket.org/creachadair/pattern"
 )
 
-func Example() {
+func ExampleParse() {
 	// A pattern consists of a template string containing pattern words, and a
 	// set of bindings that give regular expressions that each word must match.
-	p := pattern.MustParse(`Grade: ${grade}`, pattern.Binds{
+	p, err := pattern.Parse(`Grade: ${grade}`, pattern.Binds{
 		{Name: "grade", Expr: `([ABCD][-+]?|[EF])`},
 	})
-
-	// Matching verifies that a needle matches the template string with the
-	// pattern expressions interpolated. If so, it returns a list of bindings
-	// that give the matching substrings of the needle.
-	m, err := p.Match("Grade: B+")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Parse: %v", err)
 	}
 
-	fmt.Println("Bindings:")
-	for _, bind := range m {
-		fmt.Println(bind.Name, "=", bind.Expr)
-	}
-
-	// In addition, you can substitute bindings back into a pattern to produce
-	// a new string. Bindings not mentioned by p are ignored, but all the
-	// bindings that p uses must be provided.
-	s, err := p.Apply(pattern.Binds{{
-		Name: "grade",
-		Expr: "A-",
-	}})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("\nApplied:")
-	fmt.Println(s)
+	// The string representation of a pattern is the original template string
+	// from which it was parsed.
+	fmt.Println(p)
 
 	// Output:
-	// Bindings:
-	// grade = B+
-	//
-	// Applied:
-	// Grade: A-
+	// Grade: ${grade}
 }
 
 func ExampleP_Apply() {
